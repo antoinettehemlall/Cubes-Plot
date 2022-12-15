8b092f18432f835eb421e0c30fb109c36b52c774
antoinettehemlall <hemlall3000@gmail.com>
2022-12-15 12:34:30 -0500

    Create plot to represent cubed numbers

---
+++ main.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+
+x_values = list(range(5001))
+y_values = [x**3 for x in x_values]
+
+plt.style.use('seaborn-v0_8')
+fig, ax = plt.subplots()
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Greys, s=10)
+
+#Set chart title and label axes
+ax.set_title("Cubic Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Cube of Value", fontsize=14)
+
+#Set the range for each axis
+ax.axis([0, 5100, 0, 5100**3])
+
+plt.show()
